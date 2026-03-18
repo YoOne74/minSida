15788b3d36190bdb43011b51d3f84677d88a5523
YooOne74 <fyrkant44@outlook.com>
2026-03-18 11:17:09 +0100

    fixade knappar och nu kan dem röras :3

--- projekt/main/main.js
+++ projekt/main/main.js
@@ -11,13 +11,99 @@ function updateClock() {
   hours = hours.toString().padStart(2, "0");
   minutes = minutes.toString().padStart(2, "0");
 
-
   document.getElementById("time").textContent = `${hours}:${minutes}`;
-
 }
 
 // Update every second
-setInterval(updateClock, 1000);
+//setInterval(updateClock, 1000);
 
 // Run once immediately
-updateClock();
+//updateClock();
+
+// movable windows
+// stolen from:
+// https://www.w3schools.com/howto/howto_js_draggable.asp
+
+// Make the DIV element draggable:
+
+dragElement(document.getElementsByClassName("dragable")[0]);
+
+function dragElement(elmnt) {
+  var pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0;
+  if (document.getElementById("topBar")) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById("topBar").onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+    elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
+// Hide and show window things
+function clickOnIcon(name) {
+  //toggleShow(name);
+}
+
+// Source - https://stackoverflow.com/a/53939059
+// Posted by kyw
+// Retrieved 2026-03-18, License - CC BY-SA 4.0
+
+icon = document.getElementById("temp");
+
+icon.addEventListener("dblclick", (e) => {
+  toggleShow("cristofferWindow");
+});
+
+function hideWindow(name) {
+  toggleShow(name);
+}
+
+function fullScreenWindow(name) {
+  // TODO
+}
+
+function closeWindow(name) {
+  toggleShow(name);
+}
+
+function toggleShow(name) {
+  var x = document.getElementById(name);
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
